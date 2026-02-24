b2dab1d55673d59927323855404e27d630b2b920
uliana7 <115182066+uliana7@users.noreply.github.com>
2026-02-24 20:25:17 +0300

    Initial commit

---
+++ ChatServer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServer;
+
+internal static class Program
+{
+    private const int Port = 13000;
+
+    public static async Task Main()
+    {
+        var listener = new TcpListener(IPAddress.Any, Port);
+        listener.Start();
+
+        Console.WriteLine($"ChatServer started on port {Port}");
+
+        while (true)
+        {
+            TcpClient client = await listener.AcceptTcpClientAsync();
+            Console.WriteLine("Client connected: " + client.Client.RemoteEndPoint);
+
+            client.Close();
+        }
+    }
+}
